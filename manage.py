3ef4588fa620e2385b1873f72a8c45ce7f4f23e8
Benish Khan <benish@circleci.com>
2020-04-03 01:02:35 -0700

    Initial flask and flask-restx setup.

---
+++ manage.py
@@ -0,0 +1,14 @@
+"""manage.py
+#Here, we created a new FlaskGroup instance to extend the normal CLI with commands related 
+to the Flask app.
+"""
+from flask.cli import FlaskGroup
+
+from project import app
+
+
+cli = FlaskGroup(app)
+
+
+if __name__ == '__main__':
+    cli()
---
+++ project/config.py
@@ -0,0 +1,16 @@
+#we will define environment specific variables here.
+# project/config.py
+class BaseConfig:
+    TESTING = False
+
+
+class DevelopmentConfig(BaseConfig):
+    pass
+
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    pass
