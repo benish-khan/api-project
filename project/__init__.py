079fa6222b96e07cd1e669adf182b20aff019cac
Benish Khan <benish@circleci.com>
2020-04-07 17:36:36 -0700

    update config, update app

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,7 @@
 # project/__init__.py
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_restx import Resource, Api
 
 
@@ -13,12 +13,24 @@ api = Api(app)
 #set config
 app.config.from_object('project.config.DevelopmentConfig') 
 
+#add the first end-point
 class Ping(Resource):
     def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
+    	return {
+    	'status': 'success',
+    	'message': 'pong'
+    	}
 
+# def get_status_code(url):
+#     """ This function returns the status code of the url."""
+#     try:
+#         status_code = requests.get(url, timeout=30).status_code
+#         return status_code
+#     except requests.ConnectionError:
+#         return site_down
+    
 
-api.add_resource(Ping, '/ping')
+api.add_resource(Ping, '/pong')
+#api.add_resource(get_status_code, 'status')
+
+#return a 201 and modify one of the headers. This makes it easier to right a customer test. 
